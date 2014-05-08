bec8ac2e4f3926ea99c4260b990048b9d4e1f23e
Jonathan Dade <jonathandade@gmail.com>
2014-05-08 13:50:11 +0100

    Added headers processing file

--- delay.php
+++ delay.php
@@ -4,9 +4,10 @@
   </head>
   <body>
     <?php
-      echo "<p>Time:" + date(DATE_RFC2822);
+      date_default_timezone_set('UTC');
+      echo "<p>Time:".date(DATE_RFC2822);
       sleep(5);
-      echo "sleep complete" + date(DATE_RFC2822);
+      echo "sleep complete".date(DATE_RFC2822);
       echo "</p>";
     ?>
   </body>
---
+++ headers.php
@@ -0,0 +1,18 @@
+<?php
+  date_default_timezone_set('UTC');
+  if (! empty($_POST["Expires"])){
+    header("Expires: ".$_POST["Expires"])
+  }
+  if (! empty($_POST["Cache-Control"])){
+    header("Cache-Control: ".$_POST["Cache-Control"])
+  }
+  if (! empty($_POST["Surrogate-Control"])){
+    header("Surrogate-Control: ".$_POST["Surrogate-Control"])
+  }
+
+  print "<html>\n\t<head>\n";
+  print "<title>".__FILE__."</title>\n";
+  print "\n</head>\n<body>\n";
+  print ""  
+  print "\n\t</body>\n</html>";
+?>
