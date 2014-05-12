8a8b6beb632360460102e2edb703b7ba8d430c01
Jonathan Dade <jonathandade@gmail.com>
2014-05-12 16:30:17 +0100

    formatting change

--- delay.php
+++ delay.php
@@ -5,9 +5,9 @@
   print "<title>".__FILE__."</title>\n";
   print "\n</head>\n<body>\n";
 
-  echo "<p>Time:".date(DATE_RFC2822)."</p>\n";
+  echo "<p>Time: ".date(DATE_RFC2822)."</p>\n";
   sleep((int)$_GET['time']);
-  echo "<p>sleep complete".date(DATE_RFC2822)."</p>\n";
+  echo "<p>sleep complete ".date(DATE_RFC2822)."</p>\n";
   
   print "\n\t</body>\n</html>";
 ?>
