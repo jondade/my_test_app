83cc4ccbcc76ad3f9f79c5a788719970a24ced1e
Jonathan Dade <jonathandade@gmail.com>
2014-08-07 14:18:17 +0100

    minor update

--- esi-test.php
+++ esi-test.php
@@ -11,7 +11,7 @@
 		<p>The esi is inserted between here...</p>
 		<br />
 		<br />
-		<esi:inlcude src='www.jdade.me.uk/mytest.esi' />
+		<esi:include src='www.jdade.me.uk/mytest.esi' />
 		<br />
 		<br />
 	</body>
